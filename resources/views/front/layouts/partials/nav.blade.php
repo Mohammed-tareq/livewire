f6a1788a1402e5e098670a86968eb31ef7586191
Mohammed-tareq <mohtareq1999m@gmail.com>
2026-03-16 16:24:14 +0200

    Add service, team, and testimonials pages with layout updates
    - Created service.blade.php for displaying services offered.
    - Created team.blade.php to showcase team members.
    - Created testimonials.blade.php for client feedback.
    - Added app.blade.php and guest.blade.php for layout structure.
    - Implemented navigation.blade.php for site navigation.
    - Developed profile edit page and partials for updating user information, password, and account deletion.

---
+++ resources/views/front/layouts/partials/nav.blade.php
@@ -0,0 +1,27 @@
+  <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+                <a href="" class="navbar-brand p-0">
+                    <h1 class="m-0">DGital</h1>
+                    <!-- <img src="img/logo.png" alt="Logo"> -->
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav mx-auto py-0">
+                        <a href="{{ route('home') }}" class="nav-item nav-link @yield('home-active')">Home</a>
+                        <a href="{{ route('about') }}" class="nav-item nav-link @yield('about-active')">About</a>
+                        <a href="{{ route('service') }}" class="nav-item nav-link @yield('service-active')">Service</a>
+                        <a href="{{ route('project') }}" class="nav-item nav-link @yield('project-active')">Project</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu m-0">
+                                <a href="{{ route('team') }}" class="dropdown-item">Our Team</a>
+                                <a href="{{ route('testimonial') }}" class="dropdown-item">Testimonial</a>
+                                <a href="404.html" class="dropdown-item">404 Page</a>
+                            </div>
+                        </div>
+                        <a href="{{ route('contact') }}" class="nav-item nav-link @yield('contact-active')">Contact</a>
+                    </div>
+                    <a href="" class="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Get Started</a>
+                </div>
+            </nav>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+
+
+
+Route::prefix('/')->group(function () {
+    Route::view('/', 'front.pages.index')->name('home');
+    Route::view('/about', 'front.pages.about')->name('about');
+    Route::view('/service', 'front.pages.service')->name('service');
+    Route::view('/project', 'front.pages.projects')->name('project');
+    Route::view('/team', 'front.pages.team')->name('team');
+    Route::view('/testimonial', 'front.pages.testimonial')->name('testimonial');
+    Route::view('/contact', 'front.pages.contact')->name('contact');
+});
